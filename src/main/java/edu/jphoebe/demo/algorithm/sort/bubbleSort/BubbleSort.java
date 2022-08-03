9b779f91df597db73f5f76371c9ef3feaac3216b
jphoebe <abc@123>
2022-08-03 15:54:03 +0800

    feat: bubble sort

--- src/main/java/edu/jphoebe/demo/algorithm/sort/bubbleSort/BubbleSort.java
+++ src/main/java/edu/jphoebe/demo/algorithm/sort/bubbleSort/BubbleSort.java
@@ -1,8 +1,38 @@
 package edu.jphoebe.demo.algorithm.sort.bubbleSort;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONUtil;
+
 /**
  * @author 蒋时华
  * @date 2022-08-03 11:01:45
  */
 public class BubbleSort {
+
+    public static void main(String[] args) {
+
+
+        int size = 20;
+        Integer[] array = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = RandomUtil.randomInt(0, 1000);
+        }
+
+        System.out.println("排序前：" + JSONUtil.toJsonStr(array));
+
+        for (int i = 0; i < size; i++) {
+            for (int i1 = i + 1; i1 < size; i1++) {
+                if (array[i] <= array[i1]) {
+                    continue;
+                }
+                int temp = array[i];
+                array[i] = array[i1];
+                array[i1] = temp;
+            }
+        }
+
+
+        System.out.println("排序后：" + JSONUtil.toJsonStr(array));
+    }
+
 }
