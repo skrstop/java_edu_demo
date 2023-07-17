97e4fdb7e70d2288a462211151ad74db9d2de329
shihua.jiang <4086824@qq.com>
2023-07-17 17:31:30 +0800

    feat: 测试

--- src/main/java/edu/jphoebe/demo/ttl/SimpleDemo.java
+++ src/main/java/edu/jphoebe/demo/ttl/SimpleDemo.java
@@ -2,6 +2,8 @@ package edu.jphoebe.demo.ttl;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
 import com.alibaba.ttl.threadpool.TtlExecutors;
+import lombok.*;
+import lombok.experimental.Accessors;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,56 +15,75 @@ import java.util.concurrent.TimeUnit;
  */
 public class SimpleDemo {
 
-    public static void main(String[] args) {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Accessors(chain = true)
+    public static class Text {
+        private String msg;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         // 在父线程中设置
         TransmittableThreadLocal<String> context = new TransmittableThreadLocal<String>();
-//        ThreadLocal<String> context = new ThreadLocal<String>();
-//        InheritableThreadLocal<String> context = new InheritableThreadLocal<String>();
-        context.set("value-set-in-parent");
+//        ThreadLocal<Text> context = new ThreadLocal<Text>();
+//        InheritableThreadLocal<Text> context = new InheritableThreadLocal<Text>();
+
+//        Text txt = Text.builder()
+//                .msg("aaaaa")
+//                .build();
 
         // 非线程池版
         // 在子线程中可以读取，值是"value-set-in-parent"
-        new Thread(() -> {
-            String value = context.get();
-            System.out.println(value);
-            context.set("value-set-in-parent1111111");
-        }).start();
-
-        try {
-            TimeUnit.SECONDS.sleep(3);
-            String value = context.get();
-            System.out.println(value);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        context.set("aaaaa");
+//        new Thread(() -> {
+//            for (int i = 0; i < 1000; i++) {
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                String value = context.get();
+//                System.out.println(value);
+//            }
+//        }).start();
+//
+//        for (int i = 0; i < 1000; i++) {
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            context.set(String.valueOf(i));
+//        }
+//
+//        try {
+//            TimeUnit.SECONDS.sleep(3);
+////            Text value = context.get();
+////            System.out.println(value.getMsg());
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         // 线程池版
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        // 额外的处理，生成修饰了的对象executorService
-        executorService = TtlExecutors.getTtlExecutorService(executorService);
-        context.set("value-set-in-parent");
-        Runnable task = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + " || " + context.get());
-            }
-        };
-        executorService.submit(task);
-        context.set("value-set-in-parent111");
-        try {
-            TimeUnit.SECONDS.sleep(3);
-            String value = context.get();
-            System.out.println(value);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        task = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + " || " + context.get());
-            }
-        };
-        executorService.submit(task);
+        ExecutorService executorService = TtlExecutors.getTtlExecutorService(Executors.newFixedThreadPool(1));
+        context.set("aaaa");
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName() + " || " + context.get());
+        });
+        TimeUnit.SECONDS.sleep(1);
+        context.set("aaaa1");
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName() + " || " + context.get());
+        });
+        TimeUnit.SECONDS.sleep(1);
+        context.set("aaaa2");
+        executorService.submit(() -> {
+            System.out.println(Thread.currentThread().getName() + " || " + context.get());
+        });
+
     }
 
 }
